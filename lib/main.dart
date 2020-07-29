149f72f60451c4482ebc853ee3bbc2730d63ab22
mahidul-islam <mizihan84@gmail.com>
2020-07-29 22:04:17 +0600

    added profile and some

--- lib/main.dart
+++ lib/main.dart
@@ -131,7 +131,7 @@ class _MyAppState extends State<MyApp> {
         ],
       );
     }
-    if (choice == 1) {
+    if (choice == 2) {
       return Profile();
     }
     return Container(
@@ -201,27 +201,235 @@ class Profile extends StatelessWidget {
         child: Column(
           children: [
             Card(
-              child: Center(
-                child: Card(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(24.0),
-                    child: Image.asset(
-                      "assets/appimages/placeholder.120.png",
-                      fit: BoxFit.fill,
-                    ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Center(
+                        child: Card(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100.0),
+                            child: Image.asset(
+                              "assets/appimages/placeholder.120.png",
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100.0),
+                          ),
+                          margin: EdgeInsets.all(30.0),
+                          elevation: 5.0,
+                        ),
+                      ),
+                    ],
                   ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24.0),
+                  Row(
+                    children: [
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Text(
+                            "User Name",
+                            style: TextStyle(
+                              fontSize: 28,
+                              color: Colors.amber,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Icon(Icons.edit),
+                    ],
                   ),
-                  margin: EdgeInsets.all(30.0),
-                  elevation: 5.0,
-                ),
+                ],
               ),
               elevation: 16.0,
             ),
-            Card(
-              child: Row(
-                children: [],
+            Container(
+              child: Padding(
+                child: Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Attribute :",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Value",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Attribute :",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Value",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Attribute :",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Value",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Attribute :",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Value",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Attribute :",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Value",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Attribute :",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                "Value",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
               ),
             )
           ],
